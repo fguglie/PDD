da8d082257a7ecfc0cba1c6be8d5579094456728
fg250094 <fg250094@WARFG250094-1NJ>
2020-08-31 19:20:29 -0300

    Se agrega el ejercicio que quedaba pendiente de subir del 25/08

---
+++ 25-08/25-08/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _25_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Armar un programa que le pida al usuario que adivine un numero que ustedes prefijaron, si adivina le muestran un mensaje de felicitacion, caso contrario que siga participando
+            int intentos = 0;
+            int numeroIngresado = 0;
+            int ganador = 0;
+            int numeroGanador = 5;
+            Console.WriteLine("Adivine el numero del 1 al 9!. Tienes 5 intentos");  
+            while (numeroIngresado != numeroGanador && intentos <5 && ganador == 0)
+            {
+                numeroIngresado = int.Parse(Console.ReadLine());
+                intentos++;
+                if(numeroIngresado == numeroGanador)
+                {
+                    ganador = 1;
+                    Console.WriteLine("Felicitaciones, adivinaste!");
+                }
+                else
+                {
+                    if (intentos == 5)
+                    {
+                        Console.WriteLine("Perdiste, el número era 5!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error. te quedan {5-intentos} intentos");
+                    }
+                }
+                
+            }
+
+        }
+    }
+}
